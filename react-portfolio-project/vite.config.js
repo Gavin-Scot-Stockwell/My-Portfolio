bce23562e0bd150525e16e921f7483303fc417e7
Gavin <gsstockwell@gmail.com>
2025-01-21 06:57:03 -0800

    fixing out for dist

--- react-portfolio-project/vite.config.js
+++ react-portfolio-project/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'My-Portfolio/react-portfolio-project/dist', // Specify the output directory
+    outDir: 'dist', // Specify the output directory
   },
 });
 /*
