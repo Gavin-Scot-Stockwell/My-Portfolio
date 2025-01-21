4be88833d4e2424d5742678d0eab9f93142aaa3d
Gavin <gsstockwell@gmail.com>
2025-01-21 07:12:27 -0800

    getting pages to work

--- react-portfolio-project/vite.config.js
+++ react-portfolio-project/vite.config.js
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/My-Portfolio/',
+})
+/*
+export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: 'dist', // Specify the output directory
   },
 });
-/*
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
