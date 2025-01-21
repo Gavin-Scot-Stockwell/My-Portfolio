4d3bb92bb4891cfbe98164617ccb4dfebc8fc265
Gavin <gsstockwell@gmail.com>
2025-01-20 17:32:44 -0800

    correcting dist folder again

--- react-portfolio-project/vite.config.js
+++ react-portfolio-project/vite.config.js
@@ -1,3 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory
+  },
+});
+/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,3 +15,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+*/
