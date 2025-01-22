3d3b120bb5bec03ae3b79d1e00835e9d46092dd6
Gavin <gsstockwell@gmail.com>
2025-01-21 22:07:38 -0800

    updating paths for images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
       <div>
         
         <h1>Home Page</h1>
-        <img src="src\assets\Gavin boot.jpg" alt="A picture of me" width="50%" height="50%" style={{ justifyContent:'center', }}/>
+        <img src="./assets/Gavin boot.jpg" alt="A picture of me" width="50%" height="50%" style={{ justifyContent:'center', }}/>
 
         <p>
 Hello! This is my website. Poke around if you like, or not.
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,7 +5,7 @@ export default function Contact() {
 return (
       <div style={Even.format}>
         <h1>Contact</h1>
-        <img src="src\assets\Gavin boot.jpg" alt="A picture of me" width="50%" height="50%" style={{ justifyContent:'center', }}/>
+        <img src="./assets/Gavin boot.jpg" alt="A picture of me" width="50%" height="50%" style={{ justifyContent:'center', }}/>
         <p>
       <Email/>
         </p>
